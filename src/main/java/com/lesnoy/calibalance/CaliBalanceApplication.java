250d2717d5692a73e71bd61a0f42b76fb4dc953f
Lesn0y <kilriljob@gmail.com>
2023-08-28 14:21:06 +0300

    Init base project

---
+++ src/main/java/com/lesnoy/calibalance/CaliBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.lesnoy.calibalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaliBalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lesnoy/calibalance/CaliBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesnoy.calibalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaliBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
