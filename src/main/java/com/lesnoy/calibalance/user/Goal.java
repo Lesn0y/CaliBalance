8da5d10299c488cf7e1fd23cf8d7d29a5f8c94f2
Lesn0y <kilriljob@gmail.com>
2023-08-31 20:20:45 +0300

    Add field with multiplier for Goal

--- src/main/java/com/lesnoy/calibalance/user/Goal.java
+++ src/main/java/com/lesnoy/calibalance/user/Goal.java
@@ -1,9 +1,18 @@
 package com.lesnoy.calibalance.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Goal {
 
-    PUMP_UP,
-    SLIM,
-    KEEP_FIT
+    PUMP_UP(0.15f, 0.3f / 4, 0.15f / 4, 0.55f / 9),
+    KEEP_FIT(0, 0.33f / 4, 0.25f / 4, 0.42f / 9),
+    SLIM(-0.15f, 0.45f / 4, 0.2f / 4, 0.35f / 9);
 
+    private float calMultiplier;
+    private float protMultiplier;
+    private float fatMultiplier;
+    private float carbsMultiplier;
 }
