4f3355bcbcbc2cc29fe9a4415be8beafcb763d95
Lesn0y <kilriljob@gmail.com>
2023-08-31 20:20:02 +0300

    Add method for calculating stats

--- src/main/java/com/lesnoy/calibalance/user/UserService.java
+++ src/main/java/com/lesnoy/calibalance/user/UserService.java
@@ -3,17 +3,11 @@ package com.lesnoy.calibalance.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository repository;
-
-    public List<User> findAll() {
-        return repository.findAll();
-    }
 
     public User findUserByLogin(String login) throws Exception {
         return repository.findByLogin(login)
@@ -21,11 +15,29 @@ public class UserService {
     }
 
     public User saveUser(User user) {
-        return repository.save(user);
+        return repository.save(calculateStats(user));
     }
 
     public void deleteUserByLogin(String login) throws Exception {
         User user = findUserByLogin(login);
         repository.delete(user);
+    }
+
+    private User calculateStats(User user) {
+        if (user.getSex().equals(Sex.MAN)) {
+            user.setCal((10 * user.getWeight() + 6.25f * user.getHeight() - 5 * user.getAge() + 5) * user.getActivity().getMultiplier());
+        } else if (user.getSex().equals(Sex.WOMAN)){
+            user.setCal((10 * user.getWeight() + 6.25f * user.getHeight() - 5 * user.getAge() - 161) * user.getActivity().getMultiplier());
+        }
+
+        if (!user.getGoal().equals(Goal.KEEP_FIT)) {
+            user.setCal(user.getCal() * user.getGoal().getCalMultiplier());
+        }
+
+        user.setProt(user.getCal() * user.getGoal().getProtMultiplier());
+        user.setFats(user.getCal() * user.getGoal().getFatMultiplier());
+        user.setCarbs(user.getCal() * user.getGoal().getCarbsMultiplier());
+
+        return user;
     }
 }
