406d06abc415f15879480e9d33276192ffab08e2
Lesn0y <kilriljob@gmail.com>
2023-09-14 20:57:03 +0300

    Add custom Exceptions

---
+++ src/main/java/com/lesnoy/calibalance/exception/EmptyCollectionException.java
@@ -0,0 +1,8 @@
+package com.lesnoy.calibalance.exception;
+
+public class EmptyCollectionException extends Exception{
+
+    public EmptyCollectionException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/lesnoy/calibalance/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.lesnoy.calibalance.exception;
+
+public class UserAlreadyExistsException extends Exception{
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
