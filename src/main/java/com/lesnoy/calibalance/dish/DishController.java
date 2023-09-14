88a78013a831f8ee0090f3c8a29bbbff1cdf91bc
Lesn0y <kilriljob@gmail.com>
2023-09-14 20:57:34 +0300

    Refactoring

--- src/main/java/com/lesnoy/calibalance/dish/DishController.java
+++ src/main/java/com/lesnoy/calibalance/dish/DishController.java
@@ -1,13 +1,17 @@
 package com.lesnoy.calibalance.dish;
 
+import com.lesnoy.calibalance.exception.EmptyCollectionException;
+import com.lesnoy.calibalance.exception.UserNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/dishes")
@@ -16,14 +20,20 @@ public class DishController {
     private final DishService service;
 
     @GetMapping
-    public ResponseEntity<List<Dish>> findAll(@RequestParam(name = "owner", required = false) String login) {
-        List<Dish> dishes = service.findAllByOwnerLogin(login);
-        return dishes == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(dishes);
+    public ResponseEntity<List<Dish>> findAllDishes(@RequestParam(name = "owner", required = false) String login) {
+        try {
+            return ResponseEntity.ok(service.findAllDishesByOwnerLogin(login));
+        } catch (UserNotFoundException e) {
+            log.info(e.getMessage());
+            return ResponseEntity.notFound().build();
+        } catch (EmptyCollectionException e) {
+            log.info(e.getMessage());
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Dish> saveProduct(@RequestBody @Valid Dish dish) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(service.saveProduct(dish));
+    public ResponseEntity<Dish> saveDishes(@RequestBody @Valid Dish dish) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.saveProduct(dish));
     }
 }
--- src/main/java/com/lesnoy/calibalance/dish/DishService.java
+++ src/main/java/com/lesnoy/calibalance/dish/DishService.java
@@ -1,27 +1,32 @@
 package com.lesnoy.calibalance.dish;
 
+import com.lesnoy.calibalance.exception.EmptyCollectionException;
+import com.lesnoy.calibalance.exception.UserNotFoundException;
 import com.lesnoy.calibalance.user.User;
 import com.lesnoy.calibalance.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class DishService {
 
-    private final DishRepository repository;
+    private final DishRepository dishRepository;
     private final UserService userService;
 
-    public List<Dish> findAllByOwnerLogin(String login) {
-        Optional<User> owner = userService.findUserByLogin(login);
-        return owner.map(user -> repository.findAllByIdCreator(user.getId())).orElse(null);
+    public List<Dish> findAllDishesByOwnerLogin(String login) throws UserNotFoundException, EmptyCollectionException {
+        User owner = userService.findUserByLogin(login);
+        List<Dish> dishes = dishRepository.findAllByIdCreator(owner.getId());
+        if (dishes.isEmpty()) {
+            throw new EmptyCollectionException("The user has not saved any dishes yet");
+        }
+        return dishes;
     }
 
     public Dish saveProduct(Dish dish) {
-        return repository.save(dish);
+        return dishRepository.save(dish);
     }
 
 }
