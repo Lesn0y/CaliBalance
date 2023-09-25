25d83488e92f6ede201d4becd86a5dbc8e4e00e0
Lesn0y <kilriljob@gmail.com>
2023-09-25 23:19:33 +0300

    Add new Exception

---
+++ src/main/java/com/lesnoy/calibalance/exception/NoValuePresentException.java
@@ -0,0 +1,12 @@
+package com.lesnoy.calibalance.exception;
+
+public class NoValuePresentException extends Exception{
+
+    public NoValuePresentException() {
+        super();
+    }
+
+    public NoValuePresentException(String message) {
+        super(message);
+    }
+}
