9b9d3375ace277594d9f777e5c50883a9078164c
Lesn0y <kilriljob@gmail.com>
2023-12-11 12:27:53 +0300

    Add new Entity for presentation User and last Entry

---
+++ src/main/java/com/lesnoy/calibalance/user/UserInfoDTO.java
@@ -0,0 +1,18 @@
+package com.lesnoy.calibalance.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.lesnoy.calibalance.user.entry.Entry;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserInfoDTO {
+    private User user;
+    @JsonProperty("last_entry")
+    private Entry lastEntry;
+}
