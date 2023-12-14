d52bf340295b5450c5316916060a9b8dd263324a
Lesn0y <kilriljob@gmail.com>
2023-12-14 16:38:32 +0300

    Refactor logging and returned error codes

--- src/main/java/com/lesnoy/calibalance/user/UserController.java
+++ src/main/java/com/lesnoy/calibalance/user/UserController.java
@@ -1,5 +1,6 @@
 package com.lesnoy.calibalance.user;
 
+import com.lesnoy.calibalance.exception.BadParametersException;
 import com.lesnoy.calibalance.exception.EmptyCollectionException;
 import com.lesnoy.calibalance.exception.NoValuePresentException;
 import com.lesnoy.calibalance.exception.UserNotFoundException;
@@ -14,6 +15,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -29,18 +31,18 @@ public class UserController {
 
     @GetMapping("/{username}")
     public ResponseEntity<User> findUserByUsername(@PathVariable String username) {
-        log.info("Request GET \"\"/users/username\" with 'username='" + username + "' ACCEPTED");
+        log.info("Request GET /users/" + username + " ACCEPTED");
         try {
             return ResponseEntity.ok(userService.findByUsername(username));
         } catch (UserNotFoundException e) {
-            log.info(e.getMessage());
-            return ResponseEntity.notFound().build();
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 
     @PostMapping
     public ResponseEntity<User> saveUser(@RequestBody @Valid User user) {
-        log.info("Request POST \"/\" with 'user='" + user.getUsername() + "' ACCEPTED");
+        log.error("Request POST / with 'user='" + user.getUsername() + "' ACCEPTED");
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
@@ -48,7 +50,7 @@ public class UserController {
     public ResponseEntity<List<Product>> findUserProductsByName(@PathVariable String username,
                                                                 @RequestParam(name = "type-id", required = false) Integer ordinalType,
                                                                 @RequestParam(name = "name", required = false) String name) {
-        log.info("Request GET \"/{username}/products\" with 'username='" + username + "', 'ordinalType='" + ordinalType + "', 'name='" + name + "' ACCEPTED");
+        log.info("Request GET /" + username + "/products?ordinalType=" + ordinalType + "&name=" + name + " ACCEPTED");
         try {
             if (ordinalType == null && name == null) {
                 return ResponseEntity.ok(productService.findProductsByUsername(username));
@@ -57,80 +59,83 @@ public class UserController {
             } else {
                 return ResponseEntity.ok(productService.findProductsByUsernameAndType(username, ordinalType));
             }
-        } catch (UserNotFoundException | EmptyCollectionException e) {
-            log.info(e.getMessage());
-            return ResponseEntity.noContent().build();
+        } catch (EmptyCollectionException | UserNotFoundException e) {
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (BadParametersException e) {
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 
     @GetMapping("/{username}/products/{id}")
     public ResponseEntity<Product> findProductsById(@PathVariable String username,
                                                     @PathVariable int id) {
-        log.info("Request GET \"/{username}/products/{id}\" with 'username=" + username + "', 'id=" + id + "' ACCEPTED");
+        log.info("Request GET /" + username + "/products/" + id + " ACCEPTED");
         try {
             return ResponseEntity.ok(productService.findById(id));
         } catch (NoValuePresentException e) {
-            log.info(e.getMessage());
-            return ResponseEntity.notFound().build();
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 
     @PostMapping("/{username}/products")
     public ResponseEntity<Product> saveProductToUser(@PathVariable String username,
                                                      @RequestBody @Valid Product product) {
-        log.info("Request POST \"/{username}/products\" with 'username='" + username + "', product=' " + product.getName() + "' ACCEPTED");
+        log.info("Request POST /" + username + "/products , product='" + product.getName() + "' ACCEPTED");
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body(productService.saveProductToUser(product, username));
         } catch (UserNotFoundException e) {
-            log.info(e.getMessage());
-            return ResponseEntity.notFound().build();
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 
     @DeleteMapping("/{username}/products/{productId}")
     public ResponseEntity<Product> deleteProductFromUserMenu(@PathVariable String username,
                                                              @PathVariable int productId) {
-        log.info("Request DELETE \"/{username}/products/{productId}\" with 'username='"+  username + "', productId=' "+ productId + "' ACCEPTED");
+        log.info("Request DELETE /" + username + "/products/" + productId + " ACCEPTED");
         try {
             productService.deleteProductFromUser(productId, username);
             return ResponseEntity.noContent().build();
         } catch (UserNotFoundException | NoValuePresentException e) {
-            log.info(e.getMessage());
-            return ResponseEntity.notFound().build();
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 
     @GetMapping("/{username}/entries/daily")
     public ResponseEntity<List<Entry>> findTodayEntries(@PathVariable("username") String username) {
-        log.info("Request GET \"/{username}/entries/daily\" with 'username=" + username + "' ACCEPTED");
+        log.info("Request GET /" + username + "/entries/daily ACCEPTED");
         try {
             return ResponseEntity.ok(entryService.findAllTodayEntries(username));
         } catch (UserNotFoundException | EmptyCollectionException e) {
             log.error(e.getMessage());
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 
     @GetMapping("/{username}/entries/daily-last")
     public ResponseEntity<UserInfoDTO> findLastModifiedEntryWithUser(@PathVariable("username") String username) {
-        log.info("Request GET \"/{username}/entries/daily-stats\" with 'username=" + username + "' ACCEPTED");
+        log.info("Request GET /" + username + "/entries/daily-stats ACCEPTED");
         try {
             return ResponseEntity.ok(entryService.findActualUserInfo(username));
         } catch (UserNotFoundException | EmptyCollectionException e) {
             log.error(e.getMessage());
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 
     @PostMapping("/{username}/entries")
     public ResponseEntity<Entry> saveNewEntry(@PathVariable("username") String username,
-                                                    @RequestBody EntryDTO entryDTO) {
-        log.info("Request POST \"/{username}/entries\" with 'username=" + username + "', ACCEPTED");
+                                              @RequestBody EntryDTO entryDTO) {
+        log.info("Request POST /" + username + "/entries, product_id=" + entryDTO.getProductId() +  " ACCEPTED");
         try {
             return ResponseEntity.ok(entryService.saveNewEntry(entryDTO, username));
-        } catch (UserNotFoundException | NoValuePresentException | EmptyCollectionException e) {
+        } catch (UserNotFoundException | NoValuePresentException e) {
             log.error(e.getMessage());
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
 }
