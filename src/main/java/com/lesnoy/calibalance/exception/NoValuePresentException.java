2e3be56d6f9c95cb0a19af264271fc99c69f1812
Lesn0y <kilriljob@gmail.com>
2023-12-17 15:34:28 +0300

    Refactoring

--- src/main/java/com/lesnoy/calibalance/exception/NoValuePresentException.java
+++ src/main/java/com/lesnoy/calibalance/exception/NoValuePresentException.java
@@ -1,11 +1,6 @@
 package com.lesnoy.calibalance.exception;
 
 public class NoValuePresentException extends Exception{
-
-    public NoValuePresentException() {
-        super();
-    }
-
     public NoValuePresentException(String message) {
         super(message);
     }
